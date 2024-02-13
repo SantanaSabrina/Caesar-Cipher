9302c6f4056fb5743b22163106fdff447afa0906
Sabrina Santana <sabrina.santana.90@gmail.com>
2024-02-12 20:44:06 -0500

    Final Submission

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Caesar-Cipher
+
+Caesar Cipher is an encryption and decryption method where letters in a text are shifted by a certain number of places in the alphabet. 
+
+Utilizing python, I used this method to: 
+Ordered List :-
+1. Select a message and shift value (k) to encode
+2. Define two functions, one to encrypt and one to decrypt
+3. Shift each letter in the message by "k" letters. If the shift goes past the last letter in the aphabet (z), it looks back to the first letter (a)
+
+<br>This cipher takes two command-line arguments: the message to be encrypted/decrypted and the key (k).
--- caesar.py
+++ caesar.py
@@ -1,41 +1,47 @@
 import sys
 
-message = "hello world"
-k = 10
-
+#encryp message variable
 def encrypt(message, k):
+    #create empty variable to store encypted message
     encrypted = ""
-    for char in encrypted:
+    for char in message:
          #shift character by k
          new_char = ord(char) + k
+         #wrap around if shift goes past z
+         if new_char > ord('z'):
+            new_char -= 26
          #convert back to char
          new_message = chr(new_char)
          encrypted += new_message
-    return encrypt(message, key)
+    return encrypted
 
-
+#decrypt message variable
 def decrypt(message, k):
-   decrypted = ""
-   for char in decrypted:
+    #create empty variable to store decrypted message
+    decrypted = ""
+    for char in message:
          #shift character by k
          new_char = ord(char) - k
-         #convert back to char 
+         #wrap around if shift goes past a
+         if new_char < ord('a'):
+            new_char += 26
+         # Convert back to char 
          ori_message = chr(new_char)
          decrypted += ori_message
-   return decrypt(message, key)
+    return decrypted
 
 
 if __name__ == "__main__":
-    # take in first arg as word
+    #take in first arg as word
     message = sys.argv[1]
-    # take in second arg as int key
+    #take in second arg as int key
     key = int(sys.argv[2])
 
-    # encrypt your word
-    encrypted = encrypt(message, key)
+#encrypt your word
+encrypted = encrypt(message, key)
 
-    # decrypt your encrypted word
-    decrypted = decrypt(encrypted, key)
+#decrypt your encrypted word
+decrypted = decrypt(encrypted, key)
 
-    print("Your encrypted word is", encrypted)
-    print("Your decrypted word is", decrypted)
+print("Your encrypted word is", encrypted)
+print("Your decrypted word is", decrypted)
